bf27ec5cfa44974bc9d1cfba67d5c0b474e83098
Sufiyaan Rajput <sufiyaan@peoplegrove.com>
2022-07-14 22:20:01 +0400

    Change base to sass

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ import { ThemeProvider } from 'styled-components';
 import { useEffect, useContext } from 'react';
 import { useRouter } from 'next/router'
 import Script from 'next/script'
-import '../styles/global.css';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
