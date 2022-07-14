a37fe7cc654b4346de72a0f314344668c0166113
Sufiyaan Rajput <sufiyaan@peoplegrove.com>
2022-07-14 22:24:37 +0400

    Install sass

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['encrypted-tbn0.gstatic.com', 'images.unsplash.com', 'ik.imagekit.io'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
